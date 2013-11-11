8e68490631ae39661b28b0bdb3d3fccb12a599fb
YuL <YuL78@users.noreply.github.com>
2013-11-11 22:48:34 +0100

    Update copyright notice

--- pysieve.py
+++ pysieve.py
@@ -1,7 +1,26 @@
 # ****************************************************************************
+# Copyright (c) 2013 Youcef Lemsafer
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# ****************************************************************************
 # Creation date: 2013.11.11
-# Created by: Youcef L
-# Authors: Youcef L
+# Created by: Youcef Lemsafer
+# Authors: Youcef Lemsafer
 # What it is: pysieve.py version 0.1.0
 # A Python driver for my sieving work cause factoring big numbers is a lot
 # of fun.
